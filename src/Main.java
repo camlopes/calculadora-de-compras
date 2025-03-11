351ab314240be805e9261d5e686717da59b2550c
camlopes <camalopes.cat@gmail.com>
2025-03-11 16:54:49 -0300

    Desenvolvimento da logica da calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        String item;
+        double preco;
+        int quantidade;
+        double total;
+
+        System.out.print("Qual item voce gostaria de comprar? ");
+        item = scanner.nextLine();
+
+        System.out.print("Qual é o preco de cada item? ");
+        preco = scanner.nextDouble();
+
+        System.out.print("Quantos itens voce esta comprando? ");
+        quantidade = scanner.nextInt();
+
+        total = quantidade * preco;
+
+        System.out.println("\nVoce comprou " + quantidade + " " + item + "/s");
+        System.out.println("Seu total R$" + total);
+
+        scanner.close();
     }
 }
